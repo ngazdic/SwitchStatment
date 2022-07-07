d820efc7e2631b344bf6c2c9bdb1edc844228d23
Nikola Gazdic <nikola.gazdic90@gmail.com>
2022-07-07 12:06:45 -0400

    created switch statment for my favourite subject

---
+++ Switchstatment/Program.cs
@@ -0,0 +1,34 @@
+﻿
+
+
+
+
+Console.WriteLine("Guess my favourite subject(start with capital letter:)):");
+var favSchoolSubject = Console.ReadLine();
+
+switch (favSchoolSubject)
+{
+
+    case "English":
+
+        Console.WriteLine("Yay thats it");
+            break;
+
+    case "Math":
+
+        Console.WriteLine("Nope");
+        break;
+
+    case "Spanish":
+
+        Console.WriteLine("Close");
+        break;
+
+    case "History":
+        Console.WriteLine("Cool but nope");
+        break;
+
+    default:
+        Console.WriteLine("Way Off");
+        break;
+}
